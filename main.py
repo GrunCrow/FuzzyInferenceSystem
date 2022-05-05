21c5f19f1b314f4b62d53f5ee07c33591e350fd7
GrunCrow <albamrqz751@gmail.com>
2022-05-05 17:04:23 +0200

    Main file to run the code

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import FuzzyVariable as fv
+import FuzzySystem as fs
 
 # Visualize these universes and membership functions
-fv.show_graphics()
+#fv.show_graphics()
+
+ut = 6.4
+tdiff = 30
+td = 10
+ev = 160
+
+fs.take_values(ut, tdiff, td, ev)
+
